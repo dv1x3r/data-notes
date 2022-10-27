7655ff87405d42c42d4e783efdc3b5e9dea8be4e
dv1x3r <d.purvinsh@gmail.com>
2022-10-27 09:57:16 +0300

    upd folder struct

--- sql-utils/Stored Procedures/LogTimeout.sql
+++
@@ -1,7 +0,0 @@
-﻿create procedure [adf].[LogTimeout]
-as begin
-	update [adf].[Log]
-	set [EndTimeUTC] = getutcdate(), [Status] = 3
-	where [Status] = 0 and datediff(day, StartTimeUTC, getutcdate()) > 0
-	select @@ROWCOUNT as [TimeoutRows]
-end
